a892f2f2470d2ec0d8e08d532d1ab46fa22361c9
Eric Huff <thegreaterici@gmail.com>
2023-03-13 18:02:05 -0600

    fix pre_deploy paths

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -1,4 +1,6 @@
-packer_dir="~/.local/share/nvim/site/pack/packer/start/packer.nvim"
-if [[ ! -f  $packer_dir ]]; then
+#!/bin/bash
+packer_dir="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+
+if [[ ! -d  $packer_dir ]]; then
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_dir
 fi
